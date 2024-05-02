0bd00ade6f65991f72e17102b31797c67570e934
Oleg-Melnikow <oleg.melnikau@gmail.com>
2024-05-02 14:43:25 +0300

    feat: add development and production build

---
+++ webpack.dev.js
@@ -0,0 +1,25 @@
+const common = require("./webpack.config");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    compress: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+});
