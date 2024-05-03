a46e10828e593ccbf723891e6ae50438dbd46590
Oleg-Melnikow <oleg.melnikau@gmail.com>
2024-05-03 15:03:58 +0300

    feat: add husky and config for commit

---
+++ .validate-branch-namerc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  pattern:
+    /^(init|feat|fix|refactor|docs|chore)\/RSS-ECOMM-\\d+_[a-zA-Z0-9_-]+$/,
+  errorMsg:
+    "The branch you`re trying to push doesn`t match the expected convention, please rename it!",
+};
