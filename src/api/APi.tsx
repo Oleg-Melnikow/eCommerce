b72037d682e1d2ab2dfe55af0c2ce9d22282120c
Andrei Makhnach <ahdpyxa88@gmail.com>
2024-05-05 14:37:14 +0300

    feat: implement API as class

--- src/api/APi.tsx
+++ src/api/APi.tsx
@@ -1,51 +1,55 @@
 import axios, { AxiosInstance } from "axios";
 
-async function getToken(): Promise<void> {
-  try {
-    const response = await axios.post(
-      `${process.env.CTP_AUTH_URL}/oauth/token`,
-      null,
-      {
-        params: {
-          grant_type: "client_credentials",
-          scope: process.env.CTP_SCOPES,
-        },
-        auth: {
-          username: process.env.CTP_CLIENT_ID ?? "",
-          password: process.env.CTP_CLIENT_SECRET ?? "",
-        },
-      }
-    );
+export default class API {
+  instance: AxiosInstance | undefined;
 
-    if (response.status === 200) {
-      const accessToken = response.data.access_token;
-      const tokenType = response.data.token_type;
-      localStorage.setItem("ACCES_TOKEN", accessToken);
-      localStorage.setItem("TOKEN_TYPE", tokenType);
-    } else {
-      console.error(
-        `Error fetching token: ${response.status} ${response.statusText}`
-      );
-    }
-  } catch (error) {
-    if (error instanceof Error)
-      console.error(`Error fetching token: ${error.message}`);
+  constructor() {
+    this.createAPI();
   }
-}
 
-async function createAPI(): Promise<AxiosInstance | undefined> {
-  const accessToken = localStorage.getItem("ACCES_TOKEN");
-  const tokenType = localStorage.getItem("TOKEN_TYPE");
-  if (!accessToken && !tokenType) {
-    getToken().then(() => createAPI());
-  } else {
-    const API = axios.create({
-      baseURL: process.env.CTP_API_URL,
-      headers: { Authorization: `${tokenType} ${accessToken}` },
-      responseType: "json",
-    });
-    return new Promise((resolve) => {
-      resolve(API);
-    });
+  private createAPI(): void {
+    const accessToken = localStorage.getItem("ACCES_TOKEN");
+    const tokenType = localStorage.getItem("TOKEN_TYPE");
+    if (!accessToken && !tokenType) {
+      this.getToken().then(() => this.createAPI());
+    } else {
+      this.instance = axios.create({
+        baseURL: process.env.CTP_API_URL,
+        headers: { Authorization: `${tokenType} ${accessToken}` },
+        responseType: "json",
+      });
+    }
+  }
+
+  private async getToken(): Promise<void> {
+    try {
+      const response = await axios.post(
+        `${process.env.CTP_AUTH_URL}/oauth/token`,
+        null,
+        {
+          params: {
+            grant_type: "client_credentials",
+            scope: process.env.CTP_SCOPES,
+          },
+          auth: {
+            username: process.env.CTP_CLIENT_ID ?? "",
+            password: process.env.CTP_CLIENT_SECRET ?? "",
+          },
+        }
+      );
+      if (response.status === 200) {
+        const accessToken = response.data.access_token;
+        const tokenType = response.data.token_type;
+        localStorage.setItem("ACCES_TOKEN", accessToken);
+        localStorage.setItem("TOKEN_TYPE", tokenType);
+      } else {
+        console.error(
+          `Error fetching token: ${response.status} ${response.statusText}`
+        );
+      }
+    } catch (error) {
+      if (error instanceof Error)
+        console.error(`Error fetching token: ${error.message}`);
+    }
   }
 }
