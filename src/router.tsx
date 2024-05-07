40db234806e338fe388f6fcbb4adaaf66d115a75
Oleg-Melnikow <oleg.melnikau@gmail.com>
2024-05-07 12:30:08 +0300

    feat: add navigation for project

---
+++ src/router.tsx
@@ -0,0 +1,14 @@
+import { RouteObject } from "react-router-dom";
+
+const routes: RouteObject[] = [
+  { path: "/", element: <div>Main</div> },
+  { path: "/login", element: <div>Login</div> },
+  { path: "/registration", element: <div>registration</div> },
+  { path: "/about", element: <div>About</div> },
+  { path: "/profile", element: <div>User Profile</div> },
+  { path: "/catalog", element: <div>Catalog</div> },
+  { path: "/product", element: <div>Product</div> },
+  { path: "/basket", element: <div>Basket</div> },
+];
+
+export default routes;
