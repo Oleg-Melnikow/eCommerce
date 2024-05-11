befd125e85111d1c95efe3559bc5934b31382856
Reut Vladislav <reut-2011@mail.ru>
2024-05-12 00:49:47 +0300

    feat: addAccountLinkComponent-and-refactorMainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,52 +1,92 @@
 import "./MainPage.scss";
-import React, { useState, ReactElement } from "react";
+import React, { ReactElement } from "react";
 import { NavLink } from "react-router-dom";
 
 import HeaderLogo from "../../assets/HeaderLogo.png";
 import LogIn from "../../assets/LogIn.svg";
 import Register from "../../assets/Register.svg";
 import LogOut from "../../assets/LogOut.svg";
+import Basket from "../../assets/Basket.svg";
+
+import NavLinkAccount from "../../components/_Header/NavLinkAccount";
 
 function MainPage(): ReactElement {
+  const navLinks = [
+    { id: 1, path: "/", text: "Home", className: "header__nav_link-home" },
+    {
+      id: 2,
+      path: "/catalog",
+      text: "Catalog",
+      className: "header__nav_link-catalog",
+    },
+    {
+      id: 3,
+      path: "/about",
+      text: "About",
+      className: "header__nav_link-about",
+    },
+  ];
+  const navLinksToAccount = [
+    {
+      id: 1,
+      path: "/login",
+      title: "Log In",
+      className: "header__inner_link-login",
+      imgSrc: LogIn,
+      imgAlt: "LogInLogo",
+    },
+    {
+      id: 2,
+      path: "/registration",
+      title: "Register",
+      className: "header__inner_link-register",
+      imgSrc: Register,
+      imgAlt: "RegisterLogo",
+    },
+    {
+      id: 3,
+      path: "/",
+      title: "Log Out",
+      className: "header__inner_link-logout",
+      imgSrc: LogOut,
+      imgAlt: "LogOutLogo",
+    },
+
+    {
+      id: 4,
+      path: "/basket",
+      title: "Basket",
+      className: "header__inner_link-basket",
+      imgSrc: Basket,
+      imgAlt: "BasketLogo",
+    },
+  ];
+
   return (
     <div className="main-page">
       <div className="main-page__container">
         <header className="header">
           <img src={HeaderLogo} className="header_logo" alt="HeaderLogo" />
+
           <div className="header__nav">
-            <NavLink className="header__nav_link-home" to="/">
-              Home
-            </NavLink>
-            <NavLink className="header__nav_link-catalog" to="/catalog">
-              Catalog
-            </NavLink>
-            <NavLink className="header__nav_link-about" to="/about">
-              About
-            </NavLink>
+            {navLinks.map((link) => (
+              <NavLink key={link.id} className={link.className} to={link.path}>
+                {link.text}
+              </NavLink>
+            ))}
           </div>
 
           <div className="header__inner ">
-            <NavLink to="/login" title="Log In">
-              <img
-                src={LogIn}
-                alt="LogInLogo"
-                className="header__inner_link-login"
+            {navLinksToAccount.map((elem) => (
+              <NavLinkAccount
+                key={elem.id}
+                path={elem.path}
+                title={elem.title}
+                className={elem.className}
+                imgSrc={elem.imgSrc}
+                imgAlt={elem.imgAlt}
               />
-            </NavLink>
-            <NavLink to="/registration" title="Register">
-              <img
-                src={Register}
-                alt="RegisterLogo"
-                className="header__inner_link-register"
-              />
-            </NavLink>
-            <NavLink to="/" title="Log Out">
-              <img
-                src={LogOut}
-                alt="LogOutLogo"
-                className="header__inner_link-logout"
-              />
-            </NavLink>
+            ))}
           </div>
         </header>
       </div>
