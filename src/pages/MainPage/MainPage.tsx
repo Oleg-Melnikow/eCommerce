77538afd25493fff3c0d6722e4dde52c3ea8981a
Reut Vladislav <reut-2011@mail.ru>
2024-05-11 18:45:52 +0300

    feat: addHeader-Logo_navigationLinks-SettingsLinks-and-fixBagWithSelectedTag

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,54 @@
 import "./MainPage.scss";
 import React, { useState, ReactElement } from "react";
+import { NavLink } from "react-router-dom";
+
+import HeaderLogo from "../../assets/HeaderLogo.png";
+import LogIn from "../../assets/LogIn.svg";
+import Register from "../../assets/Register.svg";
+import LogOut from "../../assets/LogOut.svg";
 
 function MainPage(): ReactElement {
   return (
     <div className="main-page">
       <div className="main-page__container">
-        <h1>Header will be here!</h1>
+        <header className="header">
+          <img src={HeaderLogo} className="header_logo" alt="HeaderLogo" />
+          <div className="header__nav">
+            <NavLink className="header__nav_link-home" to="/">
+              Home
+            </NavLink>
+            <NavLink className="header__nav_link-catalog" to="/catalog">
+              Catalog
+            </NavLink>
+            <NavLink className="header__nav_link-about" to="/about">
+              About
+            </NavLink>
+          </div>
+
+          <div className="header__inner ">
+            <NavLink to="/login" title="Log In">
+              <img
+                src={LogIn}
+                alt="LogInLogo"
+                className="header__inner_link-login"
+              />
+            </NavLink>
+            <NavLink to="/registration" title="Register">
+              <img
+                src={Register}
+                alt="RegisterLogo"
+                className="header__inner_link-register"
+              />
+            </NavLink>
+            <NavLink to="/" title="Log Out">
+              <img
+                src={LogOut}
+                alt="LogOutLogo"
+                className="header__inner_link-logout"
+              />
+            </NavLink>
+          </div>
+        </header>
       </div>
     </div>
   );
