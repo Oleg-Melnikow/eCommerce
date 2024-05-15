87c697386a9a8bd23fc3ae52c31a8a57ac15a50f
Oleg-Melnikow <oleg.melnikau@gmail.com>
2024-05-15 19:23:39 +0300

    feat: add password button for input

--- src/components/InputTag/InputTag.tsx
+++ src/components/InputTag/InputTag.tsx
@@ -1,8 +1,52 @@
+import { IconButton, InputAdornment, TextField } from "@mui/material";
+import InputTagProps from "types/InputTagProps";
+import { ReactElement, useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import "./InputTag.scss";
 
-import InputTagProps from "types/InputTagProps";
+type PasswordButtonProps = {
+  showPassword: boolean;
+  handleClickShowPassword: () => void;
+  handleMouseDownPassword: (event: React.MouseEvent<HTMLButtonElement>) => void;
+};
 
-function InputTag({ type, id, onChange }: InputTagProps): JSX.Element {
+function PasswordButton(props: PasswordButtonProps): ReactElement {
+  const { showPassword, handleClickShowPassword, handleMouseDownPassword } =
+    props;
+  return (
+    <InputAdornment position="end">
+      <IconButton
+        aria-label="toggle password visibility"
+        onClick={handleClickShowPassword}
+        onMouseDown={handleMouseDownPassword}
+        edge="end"
+      >
+        {showPassword ? <VisibilityOff /> : <Visibility />}
+      </IconButton>
+    </InputAdornment>
+  );
+}
+
+function InputTag({
+  type,
+  id,
+  onChange,
+  isError,
+  message,
+  value,
+}: InputTagProps): JSX.Element {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const handleClickShowPassword = (): void => {
+    setShowPassword((show) => !show);
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ): void => {
+    event.preventDefault();
+  };
+
   let styleInput = "input";
   let placeholder = "";
   let name = "";
@@ -36,14 +80,42 @@ function InputTag({ type, id, onChange }: InputTagProps): JSX.Element {
   }
 
   return (
-    <input
-      type={type}
+    <TextField
+      variant="outlined"
+      fullWidth
+      size="small"
+      sx={{
+        "& .MuiOutlinedInput-root": {
+          "&:has(> input:-webkit-autofill)": {
+            background: "#e8f0fe",
+          },
+          "& .MuiOutlinedInput-notchedOutline": {
+            border: "none",
+          },
+        },
+      }}
+      value={value || ""}
+      type={id === "password" && !showPassword ? type : "text"}
       id={localId}
       name={name}
-      className={styleInput}
       placeholder={placeholder}
       onChange={onChange}
-      autoComplete={type === "password" ? "current-password" : "on"}
+      InputProps={{
+        className: styleInput,
+        inputProps: {
+          style: { padding: "8px 0" },
+        },
+        endAdornment: id === "password" && (
+          <PasswordButton
+            showPassword={showPassword}
+            handleClickShowPassword={handleClickShowPassword}
+            handleMouseDownPassword={handleMouseDownPassword}
+          />
+        ),
+      }}
+      FormHelperTextProps={{ sx: { margin: 0 } }}
+      error={isError}
+      helperText={message || ""}
     />
   );
 }
