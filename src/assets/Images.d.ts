5d4aac3f8cd8a3ef7c09e9db5b16fa4e2ca23eb1
Reut Vladislav <reut-2011@mail.ru>
2024-05-15 17:58:37 +0300

    fix: testingCommitFixBugOnPush

--- src/assets/Images.d.ts
+++ src/assets/Images.d.ts
@@ -1,13 +1,3 @@
-module "*.png" {
-  const value: string;
-  export default value;
-}
-
-module "*.svg" {
-  const value: string;
-  export default value;
-}
-
 declare module "*.png";
 declare module "*.jpg";
 declare module "*.svg";
