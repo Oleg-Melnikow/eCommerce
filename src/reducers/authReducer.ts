a587076c9c5577f945dc76cf46a2d3087b6e1401
Oleg-Melnikow <oleg.melnikau@gmail.com>
2024-05-17 21:18:01 +0300

    feat: add authReducer with actions

---
+++ src/reducers/authReducer.ts
@@ -0,0 +1,60 @@
+import { createContext } from "react";
+import { Customer } from "types/API/Customer";
+
+interface AuthStateType {
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  user: Customer | null;
+}
+
+export const AuthInitialState: AuthStateType = {
+  isAuthenticated: false,
+  isLoading: false,
+  user: null,
+};
+
+export const authReducer = (
+  state: AuthStateType,
+  action: ActionsType
+): AuthStateType => {
+  switch (action.type) {
+    case "auth/eCommerce/SET-IS-LOADING":
+    case "auth/eCommerce/INITIALIZE":
+    case "auth/eCommerce/SET-USER":
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const loading = (isLoading: boolean) =>
+  ({
+    type: "auth/eCommerce/SET-IS-LOADING",
+    payload: { isLoading },
+  }) as const;
+
+export const initialize = (isAuthenticated: boolean, user: Customer | null) =>
+  ({
+    type: "auth/eCommerce/INITIALIZE",
+    payload: { isAuthenticated, user },
+  }) as const;
+
+export const setUser = (user: Customer) =>
+  ({ type: "auth/eCommerce/SET-USER", payload: { user } }) as const;
+
+type ActionsType =
+  | ReturnType<typeof loading>
+  | ReturnType<typeof initialize>
+  | ReturnType<typeof setUser>;
+
+export interface AuthContextValue extends AuthStateType {
+  logoutAccount: () => void;
+}
+
+export const AuthContext = createContext<AuthContextValue>({
+  ...AuthInitialState,
+  logoutAccount: () => {},
+});
