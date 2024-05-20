e89efc6cfb20ac6bb660e07641214fbea6afb89e
Oleg-Melnikow <oleg.melnikau@gmail.com>
2024-05-20 15:51:42 +0300

    feat: add test for validation postal code

--- src/tests/validationSchemes.test.ts
+++ src/tests/validationSchemes.test.ts
@@ -1,3 +1,4 @@
+import { validatePostalCode } from "../helpers/validatePostalCode";
 import validateDateOfBirth from "../helpers/validateDateOfBirth";
 import {
   loginSchema,
@@ -44,5 +45,15 @@ describe("testing validation shcema login page", () => {
   it("Should pass with valid years", () => {
     const correctYears = "2002-11-03";
     expect(validateDateOfBirth(correctYears)).toBe(true);
+  });
+
+  it("Should pass with valid postal code", () => {
+    expect(validatePostalCode("220214", "BY")).toBe(null);
+    expect(validatePostalCode("A2T 211", "BY")).toBe(
+      "Postal code not correct, please enter formar like this 224120"
+    );
+    expect(validatePostalCode("220214", "")).toBe(
+      "Please first choose country"
+    );
   });
 });
