f493f33b62cd9e96c8897a3686d9d4c564539dfe
Andrei Makhnach <ahdpyxa88@gmail.com>
2024-05-23 21:42:01 +0300

    feat: add product details

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,11 +1,13 @@
 import { ReactElement } from "react";
 import "./ProductPage.scss";
 import Slider from "components/Slider/slider";
+import ProductDetails from "components/ProductDetails/ProductDetails";
 
 function ProductPage(): ReactElement {
   return (
     <div className="product-page">
       <Slider />
+      <ProductDetails />
     </div>
   );
 }
