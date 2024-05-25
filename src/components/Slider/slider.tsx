c81a2e60b1ac68c168be530834949f2d645dd45b
Andrei Makhnach <ahdpyxa88@gmail.com>
2024-05-25 18:04:11 +0300

    fix: fix bugs

--- src/components/Slider/slider.tsx
+++ src/components/Slider/slider.tsx
@@ -4,12 +4,20 @@ import img1 from "../../assets/product/1ab58d21463d86aa3768f5c6c78f8a1f.png";
 import img2 from "../../assets/product/90effae17498640571679183299f774e.jpeg";
 
 function Slider(): ReactElement {
+  const thumbs = [
+    { image: img1, title: "image1" },
+    { image: img2, title: "image2" },
+  ].map((item) => (
+    <img
+      key={item.title}
+      className="slider__thumb"
+      src={item.image}
+      alt={item.title}
+    />
+  ));
   return (
     <div className="slider">
-      <div className="slider__thumbs-wrapper">
-        <img className="slider__thumb" src={img1} alt="image1" />
-        <img className="slider__thumb" src={img2} alt="image2" />
-      </div>
+      <div className="slider__thumbs-wrapper">{thumbs} </div>
       <div className="slider__image-wrap">
         <img className="slider__image" src={img1} alt="imag" />
       </div>
