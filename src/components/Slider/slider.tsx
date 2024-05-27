4386e1d5719d44f418394ce1b47dd79ee09ccf34
Andrei Makhnach <ahdpyxa88@gmail.com>
2024-05-27 22:30:13 +0300

    feat: implement image sliding

--- src/components/Slider/slider.tsx
+++ src/components/Slider/slider.tsx
@@ -1,12 +1,15 @@
-import { ReactElement } from "react";
+import { ReactElement, useRef, useState } from "react";
 import "./slider.scss";
 import { ProductData } from "types/API/Product";
+import { Box, Slide } from "@mui/material";
 
 type PropsType = {
   product: ProductData;
 };
 
 function Slider({ product }: PropsType): ReactElement {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const containerRef = useRef<HTMLElement>(null);
   const { images } = product.masterData.current.masterVariant;
   const thumbs = images.map((image) => (
     <img
@@ -16,12 +19,41 @@ function Slider({ product }: PropsType): ReactElement {
       alt={image.label}
     />
   ));
+
+  const slider = images.map((image, index) => (
+    <Slide
+      key={image.url}
+      direction="left"
+      in={currentSlide === index}
+      appear={false}
+      container={containerRef.current}
+    >
+      <img className="slider__image" src={image.url} alt={image.label} />
+    </Slide>
+  ));
+
+  const handleNextSlide = (): void => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide + 1 < images.length ? prevSlide + 1 : 0
+    );
+  };
+
+  const handlePrevSlide = (): void => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide > 0 ? prevSlide - 1 : images.length - 1
+    );
+  };
+
   return (
     <div className="slider">
       <div className="slider__thumbs-wrapper">{thumbs} </div>
-      <div className="slider__image-wrap">
-        <img className="slider__image" src={thumbs[0].props.src} alt="imag" />
-      </div>
+      <Box
+        className="slider__image-wrap"
+        onClick={handleNextSlide}
+        ref={containerRef}
+      >
+        {slider}
+      </Box>
     </div>
   );
 }
