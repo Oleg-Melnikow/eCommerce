3327eeeecf6ad8cb209afa39a0ed697e93870fbc
Andrei Makhnach <ahdpyxa88@gmail.com>
2024-06-04 01:16:36 +0300

    fix: fix bug loader

--- src/contexts/ProductContext.tsx
+++ src/contexts/ProductContext.tsx
@@ -195,7 +195,7 @@ export function ProductProvider(props: ProviderProps): ReactElement {
   );
 
   const initializeCatalog = useCallback(async (): Promise<void> => {
-    dispatch(loading(false));
+    dispatch(loading(true));
     try {
       await getCategoriesData();
       dispatch(setInitialize(true));
