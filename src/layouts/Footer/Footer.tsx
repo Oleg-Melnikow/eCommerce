55d612c869a7723e223e09c2033a36b1b87f5b89
Reut Vladislav <reut-2011@mail.ru>
2024-06-11 21:04:36 +0300

    feat: addaboutpage

---
+++ src/layouts/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import "./Footer.scss";
+import { ReactElement } from "react";
+
+function Footer(): ReactElement {
+  return <footer className="footer">This is Footer</footer>;
+}
+
+export default Footer;
